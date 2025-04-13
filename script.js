30860a7c0e45ff9df25a484c9deabc6d90f689f8
SandeepKumarSolanki <166508938+SandeepKumarSolanki@users.noreply.github.com>
2025-04-13 20:59:25 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
 function checkNumber(num) {
   //your code here
+	num = +num;
+	if(num > 0){
+		if(num%2 === 0 && num%5 === 0){
+			return true;
+		}
+	}else{
+		return false;
+	}
+	return false;
 }
 
 const num = parseInt(prompt("Enter a number."));
